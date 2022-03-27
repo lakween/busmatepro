9992a9d0a873f5151dee896d2903796ae9f98c5e
lakweensenathilake <lakweensenathilake@gmail.com>
2022-03-27 13:01:16 +0530

    bug-fix

--- src/components/common/sidebar/sidebar.component.js
+++ src/components/common/sidebar/sidebar.component.js
@@ -1,22 +1,30 @@
 import {Box, Flex, Text} from "@chakra-ui/react";
-//import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
+import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
 
 const Sidebar = () => {
 
     return (
         <Flex flexDirection={'row'}>
-            <Box gap={5} display={'flex'} bg={'aliceblue'}
-                 flexDirection={'column'} width={'250px'} height={'100vh'}>
-                <Box display={'Flex'} justifyContent={'center'} alignItems={'center'} height={'10vh'}>
-                    <Text>
-                        Busmate Sri Lanka
-                    </Text>
+            <Box gap={5} display={'flex'} bg={'aliceblue'} borderStyle={'solid'} borderRadius={'md'} borderWidth={2}
+                 flexDirection={'column'} width={'20vw'} height={'100vh'}>
+                <Box bgGradient='linear(to-r, #1d82d0, #0e436d)' flexDirection={"column"} display={'Flex'} justifyContent={'center'} alignItems={'center'} height={'20vh'}>
+
+                    <Text
+                        bgGradient='linear(to-l, #7928CA, #FF0080)'
+                        bgClip='text'
+                        fontSize='3xl'
+                        fontWeight='extrabold'>Busmate Sri Lanka   </Text>
+                    <Text
+                        bgGradient='linear(to-l, #7928CA, #FF0080)'
+                        bgClip='text'
+                        fontSize='2xl'
+                        fontWeight='extrabold'>Welcome </Text>
                 </Box>
                 <NavLink/>
                 <NavLink/>
                 <NavLink/>
             </Box>
-            {/*<Outlet/>*/}
+            <Outlet/>
         </Flex>
     )
 }
--- src/components/passenger/pages/home-page/home.page.js
+++ src/components/passenger/pages/home-page/home.page.js
@@ -5,6 +5,7 @@ const Home= ()=>{
 
     return (
         <>
+            Home Page working
         </>
     )
 
