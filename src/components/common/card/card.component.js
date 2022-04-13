2c9f2d6767a18f37386e4f02613ff5b1bafc505e
lakweensenathilake <lakweensenathilake@gmail.com>
2022-04-13 13:01:52 +0530

    feat():sign up page

--- src/components/common/card/card.component.js
+++ src/components/common/card/card.component.js
@@ -1,6 +1,19 @@
+import {Box, Text} from "@chakra-ui/react";
+import React from "react";
 
+const Card = (props) => {
+    let {padding = '3', margin, border = '1px', borderColor = 'black', outSideTitle , inSideTitle ,children} = props
 
-const Card = ()=>{
-
+    return (
+        <Box padding={padding} margin={margin} borderStyle={"solid"} borderWidth={border} borderColor={"#DDDDDD"}
+             borderRadius={3}>
+            <Text>
+                jsfgsdjfgdshgfshd
+            </Text>
+            {children}
+        </Box>
+    )
 }
+
+export default Card
 
--- src/components/passenger/pages/sign-up/sign-up.page.js
+++ src/components/passenger/pages/sign-up/sign-up.page.js
@@ -1,12 +1,20 @@
-import { Container } from '@chakra-ui/react'
+import {Box, Container} from '@chakra-ui/react'
 import React from "react";
+import Card from "../../../common/card/card.component";
 
 const SignUp = () => {
     return(
-        <Container  maxW='70%'>
-            There are many benefits to a joint design and development system. Not only does it bring benefits to the design team, but it also brings benefits to
-            engineering teams. It makes sure that our experiences have a consistent look
-            and feel, not just in our design specs, but in production
+        <Container  maxW='70%' >
+            <Box>
+                <Card>
+                    <div>
+                        There are many benefits to a joint design and development system. Not only does it bring benefits to the design team, but it also brings benefits to
+                        engineering teams. It makes sure that our experiences have a consistent look
+                        and feel, not just in our design specs, but in production
+                    </div>
+                </Card>
+            </Box>
+
         </Container>
     )
 }
