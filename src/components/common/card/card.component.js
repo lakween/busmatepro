89710c0e4b3269a3616809a76533d7666df50378
lakweensenathilake <lakweensenathilake@gmail.com>
2022-04-13 13:13:41 +0530

    feat():sign up page

--- src/components/common/card/card.component.js
+++ src/components/common/card/card.component.js
@@ -2,17 +2,30 @@ import {Box, Text} from "@chakra-ui/react";
 import React from "react";
 
 const Card = (props) => {
-    let {padding = '3', margin, border = '1px', borderColor = 'black', outSideTitle , inSideTitle ,children} = props
+    let {padding = '3', margin, border = '1px', borderColor = 'black', outSideTitle, inSideTitle, children} = props
+
+    const insideTitleMarkup = (<Text size={'md'} py={2} color={'#001f3f'} fontWeight={'bold'}>
+        {inSideTitle}
+    </Text>)
+
+    const outsideTitleMarkup = (<Text size={'md'} py={2} px={3} color={'#001f3f'} fontWeight={'bold'}>
+        {'asfjhasygfjas'}
+    </Text>)
 
     return (
-        <Box padding={padding} margin={margin} borderStyle={"solid"} borderWidth={border} borderColor={"#DDDDDD"}
-             borderRadius={3}>
-            <Text>
-                jsfgsdjfgdshgfshd
-            </Text>
-            {children}
+        <Box>
+            {outsideTitleMarkup}
+            <Box padding={padding} margin={margin} borderStyle={"solid"} borderWidth={border} borderColor={"#DDDDDD"}
+                 borderRadius={3}>
+                {inSideTitle && insideTitleMarkup}
+                {children}
+            </Box>
         </Box>
+
     )
+}
+
+const InSideTitle = (props) => {
 }
 
 export default Card
