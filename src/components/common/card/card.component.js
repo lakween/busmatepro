57eb1d8a6f453fc053cfebabcb01ded64ae2a92d
lakweensenathilake <lakweensenathilake@gmail.com>
2022-04-13 13:31:28 +0530

    feat():sign up page

--- src/components/common/card/card.component.js
+++ src/components/common/card/card.component.js
@@ -4,28 +4,24 @@ import React from "react";
 const Card = (props) => {
     let {padding = '3', margin, border = '1px', borderColor = 'black', outSideTitle, inSideTitle, children} = props
 
-    const insideTitleMarkup = (<Text size={'md'} py={2} color={'#001f3f'} fontWeight={'bold'}>
+    const insideTitleMarkup = (<Text size={'md'} paddingBottom={1} color={'#001f3f'} fontWeight={'bold'}>
         {inSideTitle}
     </Text>)
 
     const outsideTitleMarkup = (<Text size={'md'} py={2} px={3} color={'#001f3f'} fontWeight={'bold'}>
-        {'asfjhasygfjas'}
+        {outSideTitle}
     </Text>)
 
     return (
-        <Box>
-            {outsideTitleMarkup}
-            <Box padding={padding} margin={margin} borderStyle={"solid"} borderWidth={border} borderColor={"#DDDDDD"}
+        <Box width={'100%'}>
+            {outSideTitle && outsideTitleMarkup}
+            <Box width={'100%'} padding={padding} margin={margin} borderStyle={"solid"} borderWidth={border} borderColor={"#DDDDDD"}
                  borderRadius={3}>
                 {inSideTitle && insideTitleMarkup}
                 {children}
             </Box>
         </Box>
-
     )
-}
-
-const InSideTitle = (props) => {
 }
 
 export default Card
--- src/components/passenger/pages/sign-up/sign-up.page.js
+++ src/components/passenger/pages/sign-up/sign-up.page.js
@@ -1,17 +1,34 @@
-import {Box, Container} from '@chakra-ui/react'
+import {Box, Container, Flex, FormControl, FormHelperText, FormLabel, Input, Text} from '@chakra-ui/react'
 import React from "react";
 import Card from "../../../common/card/card.component";
 
 const SignUp = () => {
     return(
         <Container  maxW='70%' >
-            <Box>
-                <Card>
-                    <div>
-                        There are many benefits to a joint design and development system. Not only does it bring benefits to the design team, but it also brings benefits to
-                        engineering teams. It makes sure that our experiences have a consistent look
-                        and feel, not just in our design specs, but in production
-                    </div>
+            <Box paddingTop={5}>
+                <Card inSideTitle={'Personal Informations'}>
+                    <Flex gap={3} direction={'row'}>
+                        <Box width={'100%'}>
+                            <FormControl>
+                                <FormLabel >First Name</FormLabel>
+                                <Input id='first_name' type='text' />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel >Last Name</FormLabel>
+                                <Input id='last_name' type='text' />
+                            </FormControl>
+                        </Box>
+                        <Box width={'100%'}>
+                            <FormControl>
+                                <FormLabel >State</FormLabel>
+                                <Input id='state' type='text' />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel >City</FormLabel>
+                                <Input id='city' type='text' />
+                            </FormControl>
+                        </Box>
+                    </Flex>
                 </Card>
             </Box>
 
