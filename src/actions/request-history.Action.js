de67f1c1f9233f682bf4644be1a90f8f09b4d83c
lakweensenathilake <lakweensenathilake@gmail.com>
2022-06-05 19:05:14 +0530

    feat

--- src/actions/request-history.Action.js
+++ src/actions/request-history.Action.js
@@ -1,24 +1,31 @@
 import firebase from "firebase/compat/app";
 import {collection, getDoc, getDocs, query, where} from "firebase/firestore";
 
-export const getRequests =  ()=>{
-    return async (dispatch)=>{
+export const getRequests = () => {
+    return async (dispatch) => {
         const db = firebase.firestore();
-        let arr =[]
+        let arr = []
         const user_requests = await collection(db, "user requests");
         const queryData = await query(user_requests, where("user_id", "==", 'WlpOBZh8loaBrhkoENh16xjGSrQ2'));
         const querySnapshot = await getDocs(queryData)
         for (let doc of querySnapshot.docs) {
-            // console.log(doc.data(),'arr')
-            const bus_id = await doc.data().bus_id;
-            let Busdata = await getDoc(bus_id)
-            let current_holt = await getDoc(await Busdata.data().current_holt)
-            // console.log(current_holt.data(),'arrdcdvd')
-            console.log(Busdata.data(),'arrdcdvd')
-            // const bus_id = await doc.data().bus_id;
-            // let data = await getDoc(bus_id)
-            // arr.push({...doc,...data})
-        }
 
+            let Busdata = await getDoc(doc.data().bus_id)
+            //let current_holt = await getDoc(await Busdata.data().route)
+            //let current =await getDoc(current_holt.data().current_holt)
+            //console.log((doc.data().bus_id).id,'current')
+
+            let obj = {
+                user_id: doc.data().user_id,
+                bus_id: (doc.data().bus_id).id,
+                status: doc.data().status,
+                bus_no: Busdata.data().bus_no,
+                current_holt:(await getDoc(Busdata.data().current_holt)).data().holt_name,
+                location: (await getDoc(Busdata.data().current_holt)).data().location
+            }
+            //console.log(current_holt.data(), 'arr')
+             arr.push(obj)
+        }
+        console.log(arr,'arrdcdvd')
     }
 }
