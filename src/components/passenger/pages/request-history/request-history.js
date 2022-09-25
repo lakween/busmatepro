371e60f697f500f1c01406754350535ad56823c8
lakweensenathilake <lakweensenathilake@gmail.com>
2022-09-25 21:49:30 +0530

    fix

--- src/components/passenger/pages/request-history/request-history.js
+++ src/components/passenger/pages/request-history/request-history.js
@@ -1,37 +1,8 @@
-import MUIDataTable from "mui-datatables";
-import {createTheme, ThemeProvider, unstable_createMuiStrictModeTheme} from '@material-ui/core/styles';
-import {getAuth, onAuthStateChanged} from "firebase/auth";
-import {CacheProvider} from "@emotion/react";
-import {Box, extendTheme} from '@chakra-ui/react'
-import blue from "@mui/material/colors/blue";
-import Card from "../../../common/card/card.component";
-import {getRequests} from "../../../../actions/request-history.Action";
+import {getAuth} from "firebase/auth";
+import {Box, Table, TableContainer, Tbody, Td, Th, Thead, Tr} from '@chakra-ui/react'
 import {useDispatch} from "react-redux";
 import {useEffect, useState} from "react";
-import {columns, options} from "./components/column,";
-import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-} from '@chakra-ui/react'
 import {filterDocsFromCollection, getDocFromCollection} from "../../../../actions/common.action";
-
-let themeMUI = createTheme()
-
-
-const data = [
-    ["Joe James", "Test Corp", "Yonkers", "NY"],
-    ["John Walsh", "Test Corp", "Hartford", "CT"],
-    ["Bob Herm", "Test Corp", "Tampa", "FL"],
-    ["James Houston", "Test Corp", "Dallas", "TX"],
-];
-
 
 const RequestHistory = (theme) => {
     const dispatch = useDispatch()
@@ -47,23 +18,26 @@ const RequestHistory = (theme) => {
         setRequest(requests)
     }
 
-    const BusNOCell = ({busID}) => {
+    const CustomTdGroup = ({busID, pickUpHolt}) => {
         const [state, setState] = useState({})
+
         useEffect(() => {
             getBusDetails()
         }, [])
+
         const getBusDetails = async () => {
             let result = await getDocFromCollection('bus', busID)
             let routeName = await getDocFromCollection('bus routs', result?.route_id)
-            setState({...result, routeName: routeName?.name})
+            let pickUpHoltDetails = await getDocFromCollection('bus holts', pickUpHolt)
+            setState({...result, routeName: routeName?.name, pickUpHoltName: pickUpHoltDetails?.holt_name})
 
         }
 
-        console.log(state)
         return (
             <>
                 <Td>{state?.bus_no}</Td>
                 <Td>{state?.routeName}</Td>
+                <Td>{state?.pickUpHoltName}</Td>
             </>
         )
     }
@@ -84,8 +58,7 @@ const RequestHistory = (theme) => {
                         <Tbody>
                             {requests.map((item) => (
                                 <Tr>
-                                    <BusNOCell busID={item?.bus_id}/>
-                                    <Td>{item?.pickUp_holt}</Td>
+                                    <CustomTdGroup busID={item?.bus_id} pickUpHolt={item?.pickUp_holt}/>
                                     <Td>{item?.status}</Td>
                                 </Tr>
                             ))}
