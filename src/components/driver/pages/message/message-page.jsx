b33422b68fe92dddea3983dff3d31cfbc947d42f
unknown <lakweensenathilake@gmail.com>
2023-04-05 21:24:50 +0530

    messeging

--- src/components/driver/pages/message/message-page.jsx
+++ src/components/driver/pages/message/message-page.jsx
@@ -7,12 +7,12 @@ const MessagePage = () => {
 
     let userDetails = useUserLoginInfo()
     let [messages, setMessages] = useState()
+    let [selectedMessage, setSelectedMessage] = useState(0)
     let currectSelection = useRef('')
 
     useEffect(() => {
-        if (userDetails?.id) getMessageList()
-
-    }, [])
+        if (userDetails?.id) inboxHandler()
+    }, [userDetails?.id])
 
     const inboxHandler = () => {
         currectSelection.current = 'inbox'
@@ -31,20 +31,13 @@ const MessagePage = () => {
         })
     }
 
-    async function getMessageList() {
-        filterDocsFromCollectionRT('messages', '', [['to', '==', userDetails?.id]], async (messagelist) => {
-            let rebulitMessages = await rebuildMessage(messagelist)
-            setMessages(rebulitMessages)
-        })
-    }
-
     return (
         <div className={"w-full"} style={{width: '100%'}}>
             <div className={' bg-white '}>
-                <div className={'text-center'}>
+                <div className={'text-center pt-3'} style={{fontSize:'30px'}}>
                     Message List
                 </div>
-                <div className={'row gx-0 p-3'} style={{height: '86vh'}}>
+                <div className={'row gx-0 p-3'} style={{height: '81vh'}}>
                     <div className={'col-3 px-2'}>
                         <div className={'border'} style={{height: '100%'}}>
                             <div style={{
@@ -64,13 +57,14 @@ const MessagePage = () => {
                         </div>
                     </div>
                     <div className={'col-3 border'}>
-                        {messages?.map((message) => (
+                        {messages?.map((message, index) => (
                             <div style={{
                                 cursor: 'pointer',
-                                backgroundColor: `${currectSelection.current == 'inbox' ? '#faf8f7' : ''}`
+                                backgroundColor: `${selectedMessage == index ? '#faf8f7' : ''}`
                             }} onClick={inboxHandler}
                                  className={`m-2 border border-black p-3 text-center cursor-pointer rounded-md cus-shadow`}>
-                                <div className={'text-center w-100 text-truncate text-info font-weight-bold'} style={{fontWeight:'bold'}}>
+                                <div className={'text-center w-100 text-truncate text-info font-weight-bold'}
+                                     style={{fontWeight: 'bold'}}>
                                     {message?.from}
                                 </div>
                                 <div className={'text-truncate'}>
@@ -79,8 +73,14 @@ const MessagePage = () => {
                             </div>
                         ))}
                     </div>
-                    <div className={'col-6'}>
-                        fsfdf
+                    <div className={'col-6'} style={{height: '100%'}}>
+                        <div className={'border mx-2 p-2'} style={{height: '100%'}}>
+                            <div className={'text-center'}>
+                                {
+                                    messages ? messages[selectedMessage]?.message : ''
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
