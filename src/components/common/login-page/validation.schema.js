af6cc4006f646350ae22597aedf649bd2399f888
lakween <lakweensenathilake@gmail.com>
2023-07-08 23:08:01 +0530

    validation

--- src/components/common/login-page/validation.schema.js
+++ src/components/common/login-page/validation.schema.js
@@ -1,6 +1,6 @@
 import { object, string} from 'yup';
 let loginSchema = object({
-    email: string('email is required').required('email is required'),
+    username: string('Email is required').required('Email is required'),
     password: string('password is required').required('password is required'),
 });
 
