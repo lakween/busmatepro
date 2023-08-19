3f346f44c2c0ceb86c984dbe3179792e569c14c3
lakween <lakweensenathilake@gmail.com>
2023-08-19 18:45:56 +0530

    fix

--- src/components/common/sidebar/sideBarV2.component.jsx
+++ src/components/common/sidebar/sideBarV2.component.jsx
@@ -135,16 +135,6 @@ const SidebarV2 = ({children}) => {
                     <div className="my-2 bg-gray-600 h-[1px]"></div>
                 </div>
                 <div
-                    className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white"
-                >
-                    <i className="bi bi-search text-sm"></i>
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
-                    />
-                </div>
-                <div
                     className="p-2.5 mt-3 flex-row items-center rounded-md px-4 duration-300 cursor-pointer text-white"
                 >
                     {linkItems?.map((link) => (
